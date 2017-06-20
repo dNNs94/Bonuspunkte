f23a4c977ab54c4903573667b31f0d08ab555f36
Dennis <Dennis@Dennis-PC.fritz.box>
2017-06-20 17:22:58 +0200

    ImageViewer erstellt/ Bilder hinzugefügt

---
+++ src/ImageViewer/ImageViewer.java
@@ -0,0 +1,43 @@
+package ImageViewer;
+
+import java.awt.*;
+
+import javax.swing.*;
+
+
+public class ImageViewer extends JPanel{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Image img;
+	private int width, height;
+	
+	public ImageViewer(String data, int width, int height){
+		
+		this.width = width;
+		this.height = height;
+		load(data);
+	}
+	
+	public void load(String data){
+		Toolkit tk = getToolkit();
+		img = tk.getImage(data);
+	}
+	public void paintComponent(Graphics g){
+		g.drawImage(img,  0,  0, width,  height, this);
+	}
+	public static void main(String[] args) {
+		// TODO Automatisch generierter Methodenstub
+		JFrame window = new JFrame("ImageViewer");
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setSize(640, 320);
+		window.setLayout(new BorderLayout());
+		
+		ImageViewer i = new ImageViewer("images/norris01.jpg", 640, 320);
+		window.getContentPane().add(i, BorderLayout.CENTER);
+		window.setVisible(true);
+	}
+
+}
